d71970528352d1b745936b3187079ece3bc124e0
davidmori <davidmori9327@gmail.com>
2025-08-18 12:08:43 -0700

    fix: second fix

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,6 +1,6 @@
 export const contactsData = {
     email: 'davidmori9327@gmail.com',
     github: 'https://github.com/davidmori9327',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
+    linkedIn: 'https://www.linkedin.com/in/davidmori9327/',
     devUsername: "said7388"
 }
--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -5,7 +5,7 @@ export const personalData = {
   description: "My name is DAVID MORI. Senior Full-Stack Web Application Developer with 12 years of experience designing, developing, and optimizing scalable, high-performance web applications. Skilled in full-stack development, modern JavaScript frameworks, backend architecture, and cloud integration. Proven ability to lead teams, deliver enterprise-grade solutions, and implement best practices for performance, security, and maintainability.",
   email: 'davidmori9327@gmail.com',
   github: 'https://github.com/davidmori9327',
-  linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
+  linkedIn: 'https://www.linkedin.com/in/davidmori9327/',
   devUsername: "said7388",
   resume: "https://drive.google.com/file/d/1KiUUCMS4UOH6Alg2S_ZliFyFm5CKp-fL/view?usp=drive_link"
   
